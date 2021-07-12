fd5171f7b3aa3e19e25ffc75c43d765cdfbdd55f
Remi van der Laan <rrm-remi@hotmail.com>
2021-07-12 10:54:55 +0200

    Added new dummy page

--- pages/services.js
+++ pages/services.js
@@ -1,5 +1,9 @@
 import Head from "next/head";
-import { renderMetaTags, useQuerySubscription } from "react-datocms";
+import {
+  renderMetaTags,
+  StructuredText,
+  useQuerySubscription,
+} from "react-datocms";
 import Container from "../components/container";
 import HeroPost from "../components/hero-post";
 import Intro from "../components/intro";
@@ -11,39 +15,31 @@ import { metaTagsFragment, responsiveImageFragment } from "../lib/fragments";
 export async function getStaticProps({ preview }) {
   const graphqlRequest = {
     query: `
-      {
+      query($locale: SiteLocale!) {
         site: _site {
           favicon: faviconMetaTags {
             ...metaTagsFragment
           }
         }
-        blog {
+        requestpage(locale:$locale) {
+          id
+          testtext {
+            value
+          }
+          _status
+          _firstPublishedAt
           seo: _seoMetaTags {
             ...metaTagsFragment
-          }
-        }
-        allPosts(orderBy: date_DESC, first: 20) {
-          title
-          slug
-          excerpt
-          date
-          coverImage {
-            responsiveImage(imgixParams: {fm: jpg, fit: crop, w: 2000, h: 1000 }) {
-              ...responsiveImageFragment
-            }
-          }
-          author {
-            name
-            picture {
-              url(imgixParams: {fm: jpg, fit: crop, w: 100, h: 100, sat: -100})
-            }
           }
         }
       }
 
       ${metaTagsFragment}
-      ${responsiveImageFragment}
     `,
+    variables: {
+      // TODO: change this for EN page
+      locale: "nl_NL",
+    },
     preview,
   };
 
@@ -66,33 +62,25 @@ export async function getStaticProps({ preview }) {
 
 export default function Index({ subscription }) {
   const {
-    data: { allPosts, site, blog },
+    data: { site, requestpage },
+    error,
+    loading,
   } = useQuerySubscription(subscription);
+  console.log(error, loading, requestpage);
 
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
-  const metaTags = blog.seo.concat(site.favicon);
+  const metaTags = requestpage?.seo?.concat(site.favicon) || [];
 
   return (
     <>
       <Layout preview={subscription.preview}>
         <Head>{renderMetaTags(metaTags)}</Head>
         <Container>
-          <Intro />
           <div>
-            <a href="/services">Check de diensten</a>
+            <a href="/">Home</a>
           </div>
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          <StructuredText
+            data={requestpage?.testtext || error?.toString() || ""}
+          />
         </Container>
       </Layout>
     </>
